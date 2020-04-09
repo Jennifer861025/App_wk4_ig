7e4b045f119833d0141af8a0d881cafae51265b3
Jennifer <jennifer861025@gmail.com>
2020-04-09 14:36:49 +0800

    HomeHeader

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import HomeData from "../json/Home.json";
 
-export default function HomeScreen() {
+export default function HomeScreen({navigation}) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style ={styles.container}>
+      <View style = {styles.header}>
+        <Image
+          style={styles.iconStyle}
+          source={{uri:HomeData.header[0].CameraUrl}}
+        />
+        <Image
+          style={styles.InstagramStyle}
+          source={{uri:HomeData.header[0].InstagramUrl}}
+        />
+        <Image
+          style={styles.iconStyle}
+          source={{uri:HomeData.header[0].MessageUrl}}
+        />
+      </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    backgroundColor: "#fff"
   },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems:'flex-start',
+    height:60,
+    paddingTop:28,
+    paddingLeft:15,
+    paddingRight:15
+  },
+  iconStyle: {
+    width:24,
+    height:24,
+  },
+  InstagramStyle:{
+    width:100,
+    height:28,
+  }
 });
